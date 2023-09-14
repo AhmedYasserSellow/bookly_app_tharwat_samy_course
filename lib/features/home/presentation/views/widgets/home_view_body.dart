629ac03b0953f1d8f84e8a2e0809f3dbec686508
Ahmed Yasser <116739611+AhmedYasserSellow@users.noreply.github.com>
2023-09-14 16:53:41 +0300

    add most viewed books item to home view body

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_appbar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/most_viewed_books_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -9,6 +10,7 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        MostViewedBooksItem(),
       ],
     );
   }
